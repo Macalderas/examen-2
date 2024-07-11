e54d3cd0903e3df136a5f9c001fdf9be5894d189
mynor <macalderas@scl.edu.gt>
2024-07-11 12:02:00 -0600

    fil

---
+++ header/header.js
@@ -0,0 +1,8 @@
+let header=document.querySelector(".arriba");
+header.innerHTML=`
+<header class="header">
+<h1 class="titulo">usuario@</h1>
+     <a href="" class="btn-github">github</a>
+</header>
+`;
+           

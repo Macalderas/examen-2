fbe5c6e1246f2eddf7e57b863b72c6c32b663ae0
mynor <macalderas@scl.edu.gt>
2024-07-11 14:15:56 -0600

    lo58

--- header/header.js
+++ header/header.js
@@ -1,9 +1,10 @@
 let header=document.querySelector(".arriba");
 header.innerHTML=`
-<header class="header">
-<h1 class="titulo">usuario@</h1>
-     <a href="" class="btn-github">github</a>
-
-             </header>
+  <div class="parent-container">
+        <header class="header">
+            <h1 class="titulo">usuario@</h1>
+            <a href="#" class="btn-github">github</a>
+        </header>
+    </div>
 `;
            
---
+++ section/section.js
@@ -0,0 +1,10 @@
+let section=document.querySelector(".cuadros");
+section.innerHTML=`
+   <div class="parent-container">
+        <div class="cuadros">
+            <div class="cuadro">Cuadro 1</div>
+            <div class="cuadro">Cuadro 2</div>
+            <div class="cuadro">Cuadro 3</div>
+        </div>
+    </div>
+`;

01b1e7c6b4e21910cc4d808de45fb15aa1a1d38b
mynor <macalderas@scl.edu.gt>
2024-07-11 12:11:35 -0600

    sec5

---
+++ section/section.js
@@ -0,0 +1,9 @@
+let section=document.querySelector(".cuadros");
+section.innerHTML=`
+<div class="cuadros">
+        <div class="cuadro "></div>
+        <div class="cuadro "></div>
+        <div class="cuadro "></div>
+    </div>
+           
+`;
